956fb9b34dc8b848165ee92abd6f245abbd081ef
AiraySnow <athenona@gmail.com>
2018-08-10 03:53:48 +1000

    fix

--- data/repo.sh
+++ data/repo.sh
@@ -2,7 +2,7 @@
 # Get some needed tools. coreutils for mkdir command, gnugp for the signing key, and apt-transport-https to actually connect to the repo
 apt-get update
 apt-get  --assume-yes upgrade 
-apt-get  --assume-yes install coreutils gnupg2 wget dirmngr
+apt-get  --assume-yes install coreutils gnupg wget dirmngr
 # Make the sources.list.d directory
 mkdir $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
@@ -14,7 +14,7 @@ wget https://Auxilus.github.io/auxilus.key
 apt-key add auxilus.key
 echo "deb https://termux.xeffyr.ml/ extra main x11\ndeb [trusted=yes,arch=all] https://Auxilus.github.io/ termux extras\ndeb https://termux.xeffyr.ml/ stable main" >> $PREFIX/etc/apt/sources.list
 apt-key adv --keyserver pool.sks-keyservers.net --recv 9D6D488416B493F0
-apt-key adv --keyserver pgp.mit.edu --recv 9B4E7D27395024EA5A4FC6395AAAC9E0A46BE53C
+apt-key adv --keyserver pool.sks-keyservers.net --recv 9B4E7D27395024EA5A4FC6395AAAC9E0A46BE53C
 mkdir -p $PREFIX/etc/apt/sources.list.d
 echo "deb https://grimler.se root stable" > $PREFIX/etc/apt/sources.list.d/termux-root.list
 apt update
